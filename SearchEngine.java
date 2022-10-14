6c984b68c9e70c70180b7533f3419a9862cb1827
Jae-Hyun-Cha <j4cha@ucsd.edu>
2022-10-14 00:03:20 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,46 +1,55 @@
 import java.io.IOException;
 import java.net.URI;
-
+import java.util.List;
+import java.util.ArrayList;
+ 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    ArrayList<String> Search = new ArrayList<String>();
+   // The one bit of state on the server: a number that will be manipulated by
+   // various requests.
+   ArrayList<String> SearchList = new ArrayList<>();
+ 
+   public String handleRequest(URI url) {
+           
+            if (url.getPath().equals("/"))
+            return String.format("");
 
-    public void append (String search, String next_search){
-        return search + next_search;            
-    }
-
-    public String handleRequest(URI url) {
-        if (url.getPath().equals("/add?s" + extraSearch)) {
-            return String.format(extraSearch);
-        } else if (url.getPath().equals("/search?s")) {
-            return append(extraSearch, extraSearch)
-
-
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            else if (url.getPath().contains("/add")){
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters [0].equals("s")){
+                   SearchList.add(parameters[1]);
+                   return String.format(parameters[1] + " is added.");
                 }
+            } 
+           
+            else {
+            System.out.println("Path: " + url.getPath());
+            
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                String Searching ="";
+                for(int i = 0; i < SearchList.size(); i += 1){ 
+                    if (SearchList.get(i).contains(parameters[1])){ 
+                        Searching += SearchList.get(i)+" ";
+                    }
+                }
+                return Searching;
             }
-            return "404 Not Found!";
-        }
+
+           return "404 Not Found!";
     }
-}
-
-
-class NumberServer {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
-        }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
-    }
+ 
+ 
+   class SearchEngine {
+       public static void main(String[] args) throws IOException {
+           if(args.length == 0){
+               System.out.println("Missing port number! Try any number between 1024 to 49151");
+               return;
+               }
+ 
+           int port = Integer.parseInt(args[0]);
+ 
+           Server.start(port, new Handler());
+       }
+   }
+ 
 }
